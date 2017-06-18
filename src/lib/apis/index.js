bb62aa83b66b2144337b5b920207f208ebe9a354
Xavier Riu <xriu87@gmail.com>
2017-06-18 13:28:43 +0200

    Update index.js

--- src/lib/apis/index.js
+++ src/lib/apis/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   get '_default'() { return require('./2_4'); },
   get '2.4'() { return require('./2_4'); },
-  get '1.7'() { return require('./1_7'); },
   get 'master'() { return require('./master'); },
 };
